2e6844dce0c88fd23416dd31ad49d12a13b3a0e2
Yash Agrawal <145493772+IMYashAgrawal@users.noreply.github.com>
2024-01-30 22:24:58 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -10,19 +10,15 @@
 
 int main() {
     for (int i = 1; i <= 5; i++) {
-        printf("* ");
-    }
-    printf("\n");
-    for (int i = 1; i <= 3; i++) {
-        printf("* ");
-        for(int j = 1; j <= 3; j++) {
-            printf("  ");
+        for (int j = 1; j <= 5; j++) {
+            if (i == 1 || i == 5 || j == 1 || j ==5) {
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
         }
-        printf("* ");
         printf("\n");
-    }
-    for (int i = 1; i <= 5; i++) {
-        printf("* ");
     }
     printf("\n");
     return 0;
