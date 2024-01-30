db81bd3a9e7c05c36d00543cb03dc5be5d8f1e2b
Yash Agrawal <145493772+IMYashAgrawal@users.noreply.github.com>
2024-01-30 22:32:59 +0530

    17.c

--- 17.c
+++ 17.c
@@ -9,34 +9,20 @@
 #include<stdio.h>
 
 int main() {
-    int k = 0, x = 0;
-    for(int i = 1; i <= 3; i++) {
-        for (int j = 0; j < x; j++) {
-            printf("  ");
-        }
-        x = x + 1;
-        printf("* ");
-        int z = 0;
-        for (int l = 3; k < l; l--) {
-            printf("  ");
-            z = 1;
-        }
-        k = k + 2;
-        if (z == 1) {
-            printf("* ");
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (i == j || i * j == 8 || i * j == 5) {
+                printf("* ");
+            }
+            else {
+                printf("  ");
+            }
         }
         printf("\n");
     }
-    for (int i = 1; i <= 1; i++) {
-        for (int j = 1; j <= 2; j++) {
-            printf("  ");
-            printf("* ");
-        }
-        printf("\n");
-        printf("* ");
-        for (int k = 1; k <= 3; k++) {
-            printf("  ");
-        }
+    printf("\n");
+    return 0;
+}
         printf("* ");
     }
     printf("\n");
